a6acfb0342c7479c6d04e4c4de3994481e96f7b6
JuanGimenez7 <142836425+JuanGimenez7@users.noreply.github.com>
2025-02-21 18:18:37 -0400

    sidebar agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,41 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SettingsComponent } from './settings/settings.component';
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    // HomeComponent,
-    // LoginComponent,
+    HomeComponent,
+    LoginComponent,
     HeaderComponent,
     FooterComponent,
     CommonModule,
+    SidebarComponent,
+    SettingsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   showHeader = true;
+  showSidebar = true;
 
   // Rutas donde no se mostrará el header
   private hideHeaderRoutes = ['/profile'];
 
+  // Rutas donde no se mostrará el sidebar
+  private hideSidebarRoutes = ['/home', '/login'];
+
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        // Verifica si la ruta actual está en la lista de rutas ocultas
+        // Verifica si la ruta actual está en la lista de rutas ocultas para el header
         this.showHeader = !this.hideHeaderRoutes.includes(event.url);
+
+        // Verifica si la ruta actual está en la lista de rutas ocultas para el sidebar
+        this.showSidebar = !this.hideSidebarRoutes.includes(event.url);
       }
     });
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
+  {path: 'settings', component: SettingsComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' },
 ];
