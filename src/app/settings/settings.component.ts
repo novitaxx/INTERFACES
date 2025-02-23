1dc0afc66eb2f569d4e80118837373a6046027b5
JuanGimenez7 <142836425+JuanGimenez7@users.noreply.github.com>
2025-02-22 20:55:02 -0400

    Vista previa de los colores

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  styleUrl: './settings.component.css',
 })
 export class SettingsComponent {
-
+  colorTexto = '';
+  colorFondo = '';
+  colorPrimario = '';
+  colorSecundario = '';
+  tamaFuenteParrafos = '';
+  tamaFuenteTitulos = '';
+  tamaFuenteSubtitulos = '';
+  tipoPrincipal = '';
+  tipoSecundaria = '';
 }
